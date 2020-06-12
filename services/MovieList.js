457ad40925fa32613f181dd1512e712087acfc0a
Your Name <louiskirkpatrick@googlemail.com>
2020-06-12 02:37:45 +0100

    push wensit stuff

--- services/MovieList.js
+++ services/MovieList.js
@@ -2,15 +2,18 @@ const db = require('./db.js');
 const global = require('../global.js');
 
 const table = global.MOVIE_TABLE
+const blackListTable = process.env.BLACK_LIST_TABLE
 
-module.exports.getAllMovies = async function() {
+
+module.exports.getAllMovies = async function(ta) {
 
     movies = {};
 
-    await db.fetchAllItems(table).then(data => {
+    await db.fetchAllItems(ta).then(data => {
         data.forEach((item) => {
             movies[item.id] = item.data()['title'];
-            delete movies.currentId
+            delete movies.currentId;
+
         });
     });
 
@@ -22,10 +25,11 @@ module.exports.addMovie = async function(movie) {
 
     movieAdded = false
 
-    existingId = await module.exports.checkMovieExists(movie)
+    existingId = await module.exports.checkMovieExists(movie, table)
+    blackListId = await module.exports.checkMovieExists(movie, blackListTable)
 
     await module.exports.getCurrentId().then(id => {
-        if (!existingId) {
+        if (!existingId && !blackListId) {
             id = id['value'];
             db.addItem(table, id.toString(), { title: movie });
             module.exports.updateId(++id);
@@ -35,12 +39,11 @@ module.exports.addMovie = async function(movie) {
 
     return movieAdded;
 }
-
-module.exports.checkMovieExists = async function(movie) {
+module.exports.checkMovieExists = async function(movie, ta) {
 
     id = '';
 
-    await module.exports.getAllMovies().then(movies => {
+    await module.exports.getAllMovies(ta).then(movies => {
         Object.keys(movies).forEach(key => {
             mov = movies[key].toString().toLowerCase();
             if (mov === movie.toLowerCase()) {
